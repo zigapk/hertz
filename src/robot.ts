9ced0b18220dfa318d34bf15026f28e026b26741
Žiga Patačko Koderman <ziga@zerodays.dev>
2025-10-13 19:12:09 +0200

    fix: build and type typos in package.json

--- src/robot.ts
+++
@@ -1,9 +0,0 @@
-import { ClearCore } from "llamajet-driver-ts";
-import { SerialPort } from "serialport";
-
-const port = new SerialPort({
-	path: "/dev/ttyACM0",
-	baudRate: 115200,
-});
-
-export const clearcore = new ClearCore(port);
