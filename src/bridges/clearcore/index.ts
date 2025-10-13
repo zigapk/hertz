2873d9ce6a0c0a906705d5b0d8c299f3c3fdcdc9
Žiga Patačko Koderman <ziga@zerodays.dev>
2025-10-13 19:20:55 +0200

    chore: rename peripherals

--- src/bridges/clearcore/index.ts
+++ src/bridges/clearcore/index.ts
@@ -3,11 +3,11 @@ import { DPinIn } from "./digital-pin-in.js";
 import { DPinOut } from "./digital-pin-out.js";
 import { Motor } from "./motor.js";
 
-export const peripherals = [DPinIn, DPinOut, Motor];
+export const clearCorePeripherals = [DPinIn, DPinOut, Motor];
 
 declare module "react" {
 	namespace JSX {
 		interface IntrinsicElements
-			extends IntrinsicPeripherals<(typeof peripherals)[number]> {}
+			extends IntrinsicPeripherals<(typeof clearCorePeripherals)[number]> {}
 	}
 }
