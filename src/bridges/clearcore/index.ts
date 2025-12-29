7c1ea2df0fe2c9ad7e856b18cb16149341caad3b
Žiga Patačko Koderman <ziga@zerodays.dev>
2025-12-29 22:55:10 +0100

    feat: add more type exports

--- src/bridges/clearcore/index.ts
+++ src/bridges/clearcore/index.ts
@@ -3,6 +3,11 @@ import { DPinIn } from "./digital-pin-in.js";
 import { DPinOut } from "./digital-pin-out.js";
 import { Motor } from "./motor.js";
 
+// Re-export all the types
+export * from "./digital-pin-in.js";
+export * from "./digital-pin-out.js";
+export * from "./motor.js";
+
 export const clearCorePeripherals = [DPinIn, DPinOut, Motor];
 
 declare module "react" {
